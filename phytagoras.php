46421eb51a32940c99a37592e68543d25ef72fd1
AnnaMalátková <malatkova@ece.cz>
2019-10-14 22:12:12 +0200

    úkol domácí

--- phytagoras.php
+++ phytagoras.php
@@ -1,7 +1,19 @@
 <?php
 $a = 10;
 $b = 15;
-$obdelnikobsah = $a * $b
+$obdelnikobsah = $a * $b;
+$strana = 6;
+$uhel = 60;
+$vyska = $strana * sin (deg2rad($uhel));
+$obsahtrojuhelniku = ($vyska * $strana)/2;
 ?>
-Obsah obdelníku o stranách a= <?php echo $a; ?> cm a b=<?php echo $b; ?>cm je <?php echo $obdelnikobsah; ?>cm
+<html>
+<div>
+Obsah obdelníku o stranách a= <?php echo $a; ?> cm a b= <?php echo $b; ?>cm je <?php echo ($obdelnikobsah); ?>cm2
+</div>
 
+<div>
+Rovnostranný trojúhelník o straně délky <?php echo $strana; ?> cm a úhlu <?php echo $uhel; ?>° má obsah <?php echo round($obsahtrojuhelniku); ?>cm2
+</div>
+
+</html>
